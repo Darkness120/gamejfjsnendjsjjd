ab8576426d47a6d1626e47cf61fe48ba57749bb8
Darkness120 <62604086+Darkness120@users.noreply.github.com>
2020-03-24 16:57:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,19 @@ client.on('message', msg => {
 });
 
 
+`client.on('message', message => {
+ if (message.author.bot) return;
+  
+  if (message.content === prefix + "help") {
+ message.author.send(`**Comming Soon**`)
+   message.react(`✅`)
+         
+  
+ 
+
+كود هيلب يرسل بلخاص زي برو بوت ازا كان فاتح خاص يحط ايموجي صح وازا كان مقفل خاص يحط ايموجي x
+من صنعي كثير نار يقدرون يصنعوه
+المبرمج[ @H2   , iiDanger ☬ 🍷] 
 
 
 client.login(process.env.BOT_TOKEN);
