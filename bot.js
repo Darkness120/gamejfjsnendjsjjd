061fda27e7675a076669613915411a6874751f96
Darkness120 <62604086+Darkness120@users.noreply.github.com>
2020-03-24 17:08:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,6 +25,47 @@ client.on('message', msg => {
 كود هيلب يرسل بلخاص زي برو بوت ازا كان فاتح خاص يحط ايموجي صح وازا كان مقفل خاص يحط ايموجي x
 من صنعي كثير نار يقدرون يصنعوه
 المبرمج[ @H2   , iiDanger ☬ 🍷] 
+client.on("message", async message => {
+  let emoji = {
+    right: `${client.guilds.find(r => r.id === '677267870471684096').emojis.find(e => e.name === 'rightt')}`,
+    wrong: `${client.guilds.find(r => r.id === '677267870471684096').emojis.find(e => e.name === 'falsee')}`,
+    no: `${client.guilds.find(r => r.id === '677267870471684096').emojis.find(e => e.name === 'no')}`,
+    load: `${client.guilds.find(r => r.id === '677267870471684096').emojis.find(e => e.name === 'load')}`
+  
+}
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+	
+	if(message.content.toLowerCase().startsWith(prefix + `setrole`)){
+	if(!setrole[message.guild.id]) setrole[message.guild.id] = {
+    role: "Support Team"
+}
+
+		const role = setrole[message.guild.id].role
+		let newrole = message.content.split(' ').slice(1).join(' ');
+		let thisrole = message.guild.roles.find('name', newrole);
+		let permission = message.guild.member(message.author).hasPermissions('ADMINISTRATOR');
+		 const d11x1x42x = new Discord.RichEmbed()
+     .setDescription(`${emoji.wrong} | You do not have permission for that command! If you believe this is a mistake please add a high role has \`\`ADMINISTRATOR\`\` permission to yourself.`)  
+     .setColor(embedFail);
+     if(!permission) return message.channel.send(d11x1x42x);
+     const NOTX1 = new Discord.RichEmbed()
+     .setDescription(`${emoji.load} | Usage: \`\`${prefix}setrole <name>\`\``)  
+     .setColor(embedFail);
+	if(!newrole) return message.channel.send(NOTX1);
+		  const CANT = new Discord.RichEmbed()
+     .setDescription(`${emoji.load} | I can't find this role \`\`${newrole}\`\``)  
+     .setColor(embedFail);
+		if(!thisrole) return message.channel.send(CANT);
+	  setrole[message.guild.id].role = newrole	
+		  const D1 = new Discord.RichEmbed()
+     .setDescription(`${emoji.right} | The tickets role has been set to \`\`${newrole}\`\``)  
+     .setColor(embedSuccess);
+	message.channel.send(D1);
+		fs.writeFile("./setrole.json", JSON.stringify(setrole, null, 4), err => {
+        if(err) throw err;
+          });
+	}
+});
 
 
 client.login(process.env.BOT_TOKEN);
